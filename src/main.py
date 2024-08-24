70752140baa7c98020aa67d53b048693c40f0f54
amandasimmoes <simmoesamanda@gmail.com>
2024-08-23 21:18:30 -0300

    Inserção dos atributos na construção do objeto carro. Verificação do tanque para rodar a corrida

--- src/main.py
+++ src/main.py
@@ -5,43 +5,46 @@ def operar_carro(carro : Carro):
     print('2- Desligar motor')
     print('3- Acelerar')
 
-
     op = 0
     while op not in (1, 2, 3):
         op = int(input("Digite as opcoes[1-3]: "))
 
     if op == 1:
-        carro1.ligar()
+        carro.ligar()
     elif op == 2:
-        carro1.desligar()
+        carro.desligar()
     elif op == 3:
         v = float(input("Informe a velocidade: "))
         t = float(input("Informe o tempo: "))
-        carro1.acelerar(v, t)
+        carro.acelerar(v, t)
 
     print('Infos atuais do carro')
-    print(carro1)
+    print(carro)
 
 if __name__ == "__main__":
     print('Cadastre um carro 1:')
     nm_modelo = input('Digite o modelo: ')
     nm_marca = input('Digite a marca: ')
     nm_cor = input('Digite a cor: ')
+    litros = float(input('Informe o nivel do tanque:'))
+    cm = float(input('Consumo medio:'))
 
-    carro1 = Carro(nm_modelo, nm_marca, nm_cor, 0, False)
+    carro1 = Carro(nm_modelo, nm_marca, nm_cor, 0, False,litros, cm)
 
-    if __name__ == "__main__":
-        print('Cadastre um carro 2:')
-        nm_modelo = input('Digite o modelo: ')
-        nm_marca = input('Digite a marca: ')
-        nm_cor = input('Digite a cor: ')
+    print('Cadastre um carro 2:')
+    nm_modelo = input('Digite o modelo: ')
+    nm_marca = input('Digite a marca: ')
+    nm_cor = input('Digite a cor: ')
+    litros = float(input('Informe o nivel do tanque:'))
+    cm = float(input('Consumo medio:'))
 
-    carro2 = Carro(nm_modelo, nm_marca, nm_cor, 0, False)
+    carro2 = Carro(nm_modelo, nm_marca, nm_cor, 0, False, litros, cm)
 
     '''
     Controlando o carro até ele atingir 600 Km
     '''
-    while carro1.odometro < 600 and carro2.odometro < 600:
+    while carro1.odometro < 600 and carro2.odometro < 600 and \
+            (carro1.tanque > 0 or carro2.tanque > 0):
         try:
             op_carro = 0
             while op_carro not in (1, 2):
